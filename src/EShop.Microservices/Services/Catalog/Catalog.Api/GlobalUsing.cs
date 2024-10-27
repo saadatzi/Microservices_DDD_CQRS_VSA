eb79e1b57655107e42f9bb39375e8ccd17115287
Saadatzi <saadatzi@gmail.com>
2024-10-27 10:43:48 +0100

    chore(lib): remove the carter from building Blocks - theresn't any way to register the carter of building blocks in the program.cs in the catalog project - move the carter to catalog project - add mapster to building project - GlobalUsing - register the carter, MediatR and Mapster in program.cs - change port to 5000

---
+++ src/EShop.Microservices/Services/Catalog/Catalog.Api/GlobalUsing.cs
@@ -0,0 +1,9 @@
+// <fileheader>
+//     <copyright file="GlobalUsing.cs" company="SSS">
+//         Copyright (c) 2024 SSS. All rights reserved.
+//     </copyright>
+// </fileheader>
+
+global using Carter;
+global using Mapster;
+global using MediatR;
