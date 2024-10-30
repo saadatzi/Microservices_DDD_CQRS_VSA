bf5660c972f3a6305718b17413c98707da58c955
Saadatzi <saadatzi@gmail.com>
2024-10-30 12:52:26 +0100

    feat(grpc): generate proto service class from discount proto file

--- src/EShop.Microservices/Services/Discount/Discount.Grpc/Program.cs
+++ src/EShop.Microservices/Services/Discount/Discount.Grpc/Program.cs
@@ -3,7 +3,6 @@
 //         Copyright (c) 2024 SSS. All rights reserved.
 //     </copyright>
 // </fileheader>
-using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +12,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+// app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
