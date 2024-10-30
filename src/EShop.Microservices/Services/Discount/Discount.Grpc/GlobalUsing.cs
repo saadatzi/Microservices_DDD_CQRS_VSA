bdd792addd98b33479a1b87a68b40d3d03bacaf0
Saadatzi <saadatzi@gmail.com>
2024-10-30 21:00:00 +0100

    feat(grpc): develop the GetDiscount DB retrieve coupon

--- src/EShop.Microservices/Services/Discount/Discount.Grpc/GlobalUsing.cs
+++ src/EShop.Microservices/Services/Discount/Discount.Grpc/GlobalUsing.cs
@@ -5,7 +5,9 @@
 // </fileheader>
 
 global using Discount.Grpc.Data;
+global using Discount.Grpc.Models;
 global using Discount.Grpc.Services;
 global using Grpc.Core;
+global using Mapster;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Migrations;
--- src/EShop.Microservices/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/EShop.Microservices/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -4,61 +4,71 @@
 //     </copyright>
 // </fileheader>
 
-namespace Discount.Grpc.Services
+namespace Discount.Grpc.Services;
+
+/// <summary>
+/// Provides CRUD operations for managing discount coupons via gRPC.
+/// Implements the methods defined in <see cref="DiscountProtoService.DiscountProtoServiceBase"/>.
+/// </summary>
+public class DiscountService
+    (DiscountContext dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
 {
     /// <summary>
-    /// Provides CRUD operations for managing discount coupons via gRPC.
-    /// Implements the methods defined in <see cref="DiscountProtoService.DiscountProtoServiceBase"/>.
+    /// Retrieves a discount based on the provided request.
     /// </summary>
-    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    /// <param name="request">The request containing discount details.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>A <see cref="CouponModel"/> containing discount details.</returns>
+    public async override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        /// <summary>
-        /// Retrieves a discount based on the provided request.
-        /// </summary>
-        /// <param name="request">The request containing discount details.</param>
-        /// <param name="context">The server call context.</param>
-        /// <returns>A <see cref="CouponModel"/> containing discount details.</returns>
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        var coupon = await dbContext
+            .Coupons
+            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+        if (coupon == null)
         {
-            // TODO: GetDiscount from the database
-            // Placeholder for actual logic to get a discount
-            return base.GetDiscount(request, context);
+            coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
         }
 
-        /// <summary>
-        /// Creates a new discount based on the provided request.
-        /// </summary>
-        /// <param name="request">The request containing new discount details.</param>
-        /// <param name="context">The server call context.</param>
-        /// <returns>A <see cref="CouponModel"/> representing the created discount.</returns>
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
-        {
-            // Placeholder for actual logic to create a discount
-            return base.CreateDiscount(request, context);
-        }
+        logger.LogInformation($"Discount is retrieved for productName : {coupon.ProductName}, amount: {coupon.Amount}");
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel!;
+    }
 
-        /// <summary>
-        /// Updates an existing discount based on the provided request.
-        /// </summary>
-        /// <param name="request">The request containing updated discount details.</param>
-        /// <param name="context">The server call context.</param>
-        /// <returns>A <see cref="CouponModel"/> representing the updated discount.</returns>
-        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
-        {
-            // Placeholder for actual logic to update a discount
-            return base.UpdateDiscount(request, context);
-        }
+    /// <summary>
+    /// Creates a new discount based on the provided request.
+    /// </summary>
+    /// <param name="request">The request containing new discount details.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>A <see cref="CouponModel"/> representing the created discount.</returns>
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        // Placeholder for actual logic to create a discount
+        return base.CreateDiscount(request, context);
+    }
 
-        /// <summary>
-        /// Deletes a discount based on the provided request.
-        /// </summary>
-        /// <param name="request">The request containing the discount identifier to delete.</param>
-        /// <param name="context">The server call context.</param>
-        /// <returns>A <see cref="DeleteDiscountResponse"/> indicating success or failure.</returns>
-        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
-        {
-            // Placeholder for actual logic to delete a discount
-            return base.DeleteDiscount(request, context);
-        }
+    /// <summary>
+    /// Updates an existing discount based on the provided request.
+    /// </summary>
+    /// <param name="request">The request containing updated discount details.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>A <see cref="CouponModel"/> representing the updated discount.</returns>
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        // Placeholder for actual logic to update a discount
+        return base.UpdateDiscount(request, context);
+    }
+
+    /// <summary>
+    /// Deletes a discount based on the provided request.
+    /// </summary>
+    /// <param name="request">The request containing the discount identifier to delete.</param>
+    /// <param name="context">The server call context.</param>
+    /// <returns>A <see cref="DeleteDiscountResponse"/> indicating success or failure.</returns>
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        // Placeholder for actual logic to delete a discount
+        return base.DeleteDiscount(request, context);
     }
 }
