6a59c9842f154dac563949b15ac2988029192edb
Saadatzi <saadatzi@gmail.com>
2024-10-30 18:40:01 +0100

    feat(sqlite): create sqlite database using EF Core Design - .net cli way of updating database

---
+++ src/EShop.Microservices/Services/Discount/Discount.Grpc/Migrations/20241030171629_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿// <fileheader>
+//     <copyright file="20241030171629_InitialCreate.cs" company="SSS">
+//         Copyright (c) 2024 SSS. All rights reserved.
+//     </copyright>
+// </fileheader>
+#nullable disable
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductName = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
